a161530ff0154de33fc0e850bd7d9c2ad83de891
abrahamcabrera490 <55775782+abrahamcabrera490@users.noreply.github.com>
2021-06-16 18:24:28 -0500

    hoja de falla resuelta
    se agrego hoja de falla resuelta y algunos estilos para esta

---
+++ resources/views/tickets/order.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>orden
+    </title>
+
+<style>
+.conte1
+{
+    width: 1110px;
+    height: 300px;
+    margin: 0px auto;
+    text-align: center;
+    border-style: solid;
+    border-color: #000;
+}
+.conte2
+{
+    width: 1110px;
+    height: 700px;
+    margin: 0px auto;
+    text-align: center;
+    border-style: solid;
+    border-color: #000;
+}
+.fecha1
+{
+    margin-left: -50rem !important;
+}
+.fecha2
+{
+    margin-left: 50rem !important;
+    margin-top: -50px;
+}
+
+
+</style>
+
+</head>
+<body>
+    
+<header class="conte1">
+<h1>Orden de servicio Sistemas</h1>
+
+<p class="fecha1"> <strong>fecha solicitada por el usuario:</strong>  <br> 12/02/2021</p>
+<p class="fecha2">  <strong>fecha de ejecucion:</strong>  <br> 12/02/2021</p>
+<h2> <strong> Descripcion de la tarea </strong> <br></h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus quidem, maxime rerum molestiae explicabo tenetur saepe eos eveniet quaerat voluptatem culpa iure eligendi. Reiciendis tenetur nulla sint exercitationem. Error, aperiam!</p>
+</header>
+
+<div class="conte2">
+<h2>Material utilizado</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ab ratione fugiat deserunt temporibus esse omnis commodi eligendi sapiente cum ullam quo ex possimus totam, eius reprehenderit mollitia ea quod.</p>
+<h2>Observaciones</h2>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis unde deserunt optio natus alias nisi sapiente soluta inventore, voluptatum tempore quidem velit laudantium qui reprehenderit. Earum ullam alias aspernatur iure.</p>
+<br><br>
+<br>
+<img src=""  width="250" height="250" alt="">
+<hr width="250" >
+<p>Firma usuaruio conforme <br>Nombre del usuario</p>
+</div>
+
+
+
+</body>
+</html>
