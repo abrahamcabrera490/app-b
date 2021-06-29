9d848ed04f6d0d5af6d7a7664b09da6865515571
abrahamcabrera490 <55775782+abrahamcabrera490@users.noreply.github.com>
2021-06-29 16:45:01 -0500

    Se crearon Migraciones para BDD
    Se creo tablas para los indicadores de cada departamento

---
+++ database/migrations/2021_06_29_192019_create__almacen_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlmacenTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('almacen', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('user_name', 300);
+            $table->string('description', 500);
+            $table->double('eventos',10,2);
+            $table->text('observaciones');
+            $table->date('fecha');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('almacen');
+    }
+}
--- database/migrations/2021_06_29_192842_create_produccion_table.php
+++ database/migrations/2021_06_29_192842_create_produccion_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateProduccionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,14 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('produccion', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('user_name', 300);
+            $table->string('description', 500);
+            $table->double('eventos',10,2);
+            $table->text('observaciones');
+            $table->date('fecha');
         });
     }
 
@@ -31,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('produccion');
     }
 }
